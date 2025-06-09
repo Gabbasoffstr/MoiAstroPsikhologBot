df0f2f15d677de41de2b8c7d01fd1df61eb17577
Gabbasoff <gabbasoffstr@gmail.com>
2025-06-09 20:56:45 +0500

    обновление: все пожелания добавлены

--- main.py
+++ main.py
@@ -146,6 +146,9 @@ users[user_id] = {
     "dt_utc": dt
 }
 
+except Exception as e:
+    await message.answer(f"❌ Ошибка: {e}")
+
         await message.answer("✅ Готово. Хочешь подробный отчёт? Нажми 📄 Заказать подробный отчёт")
         
     except Exception as e:
